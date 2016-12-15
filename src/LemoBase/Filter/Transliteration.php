5dd9f375a1f94e491d1270ac13ea2e77f832d8d6
Matej Szendi <matej.szendi@bcom.cz>
2016-12-15 12:28:51 +0100

    * refactoring of Transliteration Filter

--- src/LemoBase/Filter/Transliteration.php
+++ src/LemoBase/Filter/Transliteration.php
@@ -2,9 +2,7 @@
 
 namespace LemoBase\Filter;
 
-use Traversable;
 use Zend\Filter\AbstractFilter;
-use Zend\Stdlib\ArrayUtils;
 
 class Transliteration extends AbstractFilter
 {
@@ -14,45 +12,6 @@ class Transliteration extends AbstractFilter
     const UNIQUE_ID_PREFIX = '__LemoBase_Filter_Transliteration__';
 
     /**
-     * Encoding for the input string
-     *
-     * @var string
-     */
-    protected $encoding;
-
-    /**
-     *  Constructor
-     *
-     * Allowed options are
-     *     'encoding'   => Input character encoding
-     *
-     * @param  string|array|Traversable $options
-     * @return void
-     */
-    public function __construct($options = null)
-    {
-        if ($options instanceof Traversable) {
-            $options = ArrayUtils::iteratorToArray($options);
-        }
-        if (!is_array($options)) {
-            $options = func_get_args();
-            $temp    = array();
-            if (!empty($options)) {
-                $temp['encoding'] = array_shift($options);
-            }
-            $options = $temp;
-        }
-
-        if (!array_key_exists('encoding', $options) && function_exists('mb_internal_encoding')) {
-            $options['encoding'] = 'UTF-8';
-        }
-
-        if (array_key_exists('encoding', $options)) {
-            $this->setEncoding($options['encoding']);
-        }
-    }
-
-    /**
      * Defined by Zend\Filter\FilterInterface
      *
      * @param  string $value
@@ -60,36 +19,12 @@ class Transliteration extends AbstractFilter
      */
     public function filter($value)
     {
-        if(setlocale(LC_ALL, '0') == 'C') {
+        if (setlocale(LC_ALL, '0') == 'C') {
             setlocale(LC_ALL, \Locale::getDefault());
         }
 
-        $value = iconv($this->getEncoding(), 'ASCII//TRANSLIT', $value);
-        $value = str_replace("'", '', $value);
+        $transliterator = \Transliterator::createFromRules(':: Any-Latin; :: Latin-ASCII;', \Transliterator::FORWARD);
 
-        return $value;
-    }
-
-    /**
-     * Set input character encoding
-     *
-     * @param  string $encoding
-     * @return Sanitize
-     */
-    public function setEncoding($encoding)
-    {
-        $this->encoding = $encoding;
-
-        return $this;
-    }
-
-    /**
-     * Return input character encoding
-     *
-     * @return string
-     */
-    public function getEncoding()
-    {
-        return $this->encoding;
+        return $transliterator->transliterate($value);
     }
 }
