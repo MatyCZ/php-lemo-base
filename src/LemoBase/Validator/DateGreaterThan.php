86e799985ba56372f3e4a4e66ec378e4b736b069
Matej Szendi <matej.szendi@bcom.cz>
2018-09-19 08:23:23 +0200

    Fix validators DateGreaterThan and DateLessThan

--- src/LemoBase/Validator/DateGreaterThan.php
+++ src/LemoBase/Validator/DateGreaterThan.php
@@ -8,156 +8,148 @@ use Zend\Validator\AbstractValidator;
 
 class DateGreaterThan extends AbstractValidator
 {
-	const NOT_GREATER           = 'notDateGreaterThan';
-	const NOT_GREATER_INCLUSIVE = 'notDateGreaterThanInclusive';
+    const NOT_GREATER           = 'notDateGreaterThan';
+    const NOT_GREATER_INCLUSIVE = 'notDateGreaterThanInclusive';
 
-	/**
-	 * Validation failure message template definitions
-	 *
-	 * @var array
-	 */
-	protected $messageTemplates = array(
-		self::NOT_GREATER           => "The input is not greater than date '%token%'",
-		self::NOT_GREATER_INCLUSIVE => "The input is not greater or equal than date '%token%'"
-	);
+    /**
+     * Validation failure message template definitions
+     *
+     * @var array
+     */
+    protected $messageTemplates = [
+        self::NOT_GREATER           => "The input is not greater than date '%min%'",
+        self::NOT_GREATER_INCLUSIVE => "The input is not greater or equal than date '%min%'",
+    ];
 
-	/**
-	 * Additional variables available for validation failure messages
-	 *
-	 * @var array
-	 */
-	protected $messageVariables = array(
-		'token' => 'token'
-	);
+    /**
+     * Additional variables available for validation failure messages
+     *
+     * @var array
+     */
+    protected $messageVariables = [
+        'min' => 'min',
+    ];
 
-	/**
-	 * Maximum value as date or field name
-	 *
-	 * @var mixed
-	 */
-	protected $token;
+    /**
+     * Maximum value as date or field name
+     *
+     * @var mixed
+     */
+    protected $min;
 
-	/**
-	 * Whether to do inclusive comparisons, allowing equivalence to max
-	 *
-	 * If false, then strict comparisons are done, and the value may equal
-	 * the max option
-	 *
-	 * @var boolean
-	 */
-	protected $inclusive;
+    /**
+     * Whether to do inclusive comparisons, allowing equivalence to min
+     *
+     * If false, then strict comparisons are done, and the value may equal
+     * the max option
+     *
+     * @var boolean
+     */
+    protected $inclusive;
 
-	/**
-	 * Sets validator options
-	 *
-	 * @param  array|Traversable $options
-	 * @throws \Zend\Validator\Exception\InvalidArgumentException
-	 */
-	public function __construct($options = null)
-	{
-		if ($options instanceof Traversable) {
-			$options = ArrayUtils::iteratorToArray($options);
-		}
-		if (!is_array($options)) {
-			$options = func_get_args();
-			$temp['token'] = array_shift($options);
+    /**
+     * Sets validator options
+     *
+     * @param  array|Traversable $options
+     * @throws \Zend\Validator\Exception\InvalidArgumentException
+     */
+    public function __construct($options = null)
+    {
+        if ($options instanceof Traversable) {
+            $options = ArrayUtils::iteratorToArray($options);
+        }
+        if (!is_array($options)) {
+            $options = func_get_args();
+            $temp['min'] = array_shift($options);
 
-			if (!empty($options)) {
-				$temp['inclusive'] = array_shift($options);
-			}
+            if (!empty($options)) {
+                $temp['inclusive'] = array_shift($options);
+            }
 
-			$options = $temp;
-		}
+            $options = $temp;
+        }
 
-		if (!array_key_exists('token', $options)) {
-			throw new \Zend\Validator\Exception\InvalidArgumentException("Missing option 'token'");
-		}
+        if (!array_key_exists('min', $options)) {
+            throw new \Zend\Validator\Exception\InvalidArgumentException("Missing option 'min'");
+        }
 
-		if (!array_key_exists('inclusive', $options)) {
-			$options['inclusive'] = false;
-		}
+        if (!array_key_exists('inclusive', $options)) {
+            $options['inclusive'] = false;
+        }
 
-		$this->setToken($options['token'])
-			->setInclusive($options['inclusive']);
+        $this->setMin($options['min'])
+            ->setInclusive($options['inclusive']);
 
-		parent::__construct($options);
-	}
+        parent::__construct($options);
+    }
 
-	/**
-	 * Returns the max option
-	 *
-	 * @return mixed
-	 */
-	public function getToken()
-	{
-		return $this->token;
-	}
+    /**
+     * Returns the max option
+     *
+     * @return mixed
+     */
+    public function getMin()
+    {
+        return $this->min;
+    }
 
-	/**
-	 * Sets the max option
-	 *
-	 * @param  mixed $max
-	 * @return GreaterThan Provides a fluent interface
-	 */
-	public function setToken($max)
-	{
-		$this->token = $max;
-		return $this;
-	}
+    /**
+     * Sets the max option
+     *
+     * @param  mixed $min
+     * @return GreaterThan Provides a fluent interface
+     */
+    public function setMin($min)
+    {
+        $this->min = $min;
+        return $this;
+    }
 
-	/**
-	 * Returns the inclusive option
-	 *
-	 * @return boolean
-	 */
-	public function getInclusive()
-	{
-		return $this->inclusive;
-	}
+    /**
+     * Returns the inclusive option
+     *
+     * @return boolean
+     */
+    public function getInclusive()
+    {
+        return $this->inclusive;
+    }
 
-	/**
-	 * Sets the inclusive option
-	 *
-	 * @param  boolean $inclusive
-	 * @return GreaterThan Provides a fluent interface
-	 */
-	public function setInclusive($inclusive)
-	{
-		$this->inclusive = $inclusive;
-		return $this;
-	}
+    /**
+     * Sets the inclusive option
+     *
+     * @param  boolean $inclusive
+     * @return GreaterThan Provides a fluent interface
+     */
+    public function setInclusive($inclusive)
+    {
+        $this->inclusive = $inclusive;
+        return $this;
+    }
 
-	/**
-	 * Returns true if and only if $value is greater than max option, inclusively
-	 * when the inclusive option is true
-	 *
-	 * @param  mixed $value
-	 * @return boolean
-	 */
-	public function isValid($value, $context = null)
-	{
-		$this->setValue($value);
+    /**
+     * Returns true if and only if $value is greater than min option, inclusively
+     * when the inclusive option is true
+     *
+     * @param  mixed $value
+     * @return boolean
+     */
+    public function isValid($value)
+    {
+        $this->setValue($value);
 
-		if (null !== $context && !strtotime($this->token) && array_key_exists($this->token, $context)) {
-			$this->token = $context[$this->token];
-		}
+        if ($this->inclusive) {
+            if (strtotime($value) < strtotime($this->min)) {
+                $this->error(self::NOT_GREATER_INCLUSIVE);
+                return false;
+            }
+        } else {
+            if (strtotime($value) <= strtotime($this->min)) {
+                $this->error(self::NOT_GREATER);
+                return false;
+            }
+        }
 
-		if (empty($this->token)) {
-			return true;
-		}
-
-		if ($this->inclusive) {
-			if (strtotime($value) < strtotime($this->token)) {
-				$this->error(self::NOT_GREATER_INCLUSIVE);
-				return false;
-			}
-		} else {
-			if (strtotime($value) <= strtotime($this->token)) {
-				$this->error(self::NOT_GREATER);
-				return false;
-			}
-		}
-
-		return true;
-	}
+        return true;
+    }
 }
--- src/LemoBase/Validator/DateLessThan.php
+++ src/LemoBase/Validator/DateLessThan.php
@@ -8,156 +8,148 @@ use Zend\Validator\AbstractValidator;
 
 class DateLessThan extends AbstractValidator
 {
-	const NOT_LESS           = 'notDateLessThan';
-	const NOT_LESS_INCLUSIVE = 'notDateLessThanInclusive';
+    const NOT_LESS           = 'notDateLessThan';
+    const NOT_LESS_INCLUSIVE = 'notDateLessThanInclusive';
 
-	/**
-	 * Validation failure message template definitions
-	 *
-	 * @var array
-	 */
-    protected $messageTemplates = array(
-		self::NOT_LESS           => "The input is not less than date '%token%'",
-		self::NOT_LESS_INCLUSIVE => "The input is not less or equal than date '%token%'"
-    );
+    /**
+     * Validation failure message template definitions
+     *
+     * @var array
+     */
+    protected $messageTemplates = [
+        self::NOT_LESS           => "The input is not less than date '%max%'",
+        self::NOT_LESS_INCLUSIVE => "The input is not less or equal than date '%max%'",
+    ];
 
-	/**
-	 * Additional variables available for validation failure messages
-	 *
-	 * @var array
-	 */
-	protected $messageVariables = array(
-		'token' => 'token'
-	);
+    /**
+     * Additional variables available for validation failure messages
+     *
+     * @var array
+     */
+    protected $messageVariables = [
+        'max' => 'max',
+    ];
 
-	/**
-	 * Maximum value as date or field name
-	 *
-	 * @var mixed
-	 */
-	protected $token;
+    /**
+     * Maximum value as date or field name
+     *
+     * @var mixed
+     */
+    protected $max;
 
-	/**
-	 * Whether to do inclusive comparisons, allowing equivalence to max
-	 *
-	 * If false, then strict comparisons are done, and the value may equal
-	 * the max option
-	 *
-	 * @var boolean
-	 */
-	protected $inclusive;
+    /**
+     * Whether to do inclusive comparisons, allowing equivalence to max
+     *
+     * If false, then strict comparisons are done, and the value may equal
+     * the max option
+     *
+     * @var boolean
+     */
+    protected $inclusive;
 
-	/**
-	 * Sets validator options
-	 *
-	 * @param  array|Traversable $options
-	 * @throws \Zend\Validator\Exception\InvalidArgumentException
-	 */
-	public function __construct($options = null)
-	{
-		if ($options instanceof Traversable) {
-			$options = ArrayUtils::iteratorToArray($options);
-		}
-		if (!is_array($options)) {
-			$options = func_get_args();
-			$temp['token'] = array_shift($options);
+    /**
+     * Sets validator options
+     *
+     * @param  array|Traversable $options
+     * @throws \Zend\Validator\Exception\InvalidArgumentException
+     */
+    public function __construct($options = null)
+    {
+        if ($options instanceof Traversable) {
+            $options = ArrayUtils::iteratorToArray($options);
+        }
+        if (!is_array($options)) {
+            $options = func_get_args();
+            $temp['max'] = array_shift($options);
 
-			if (!empty($options)) {
-				$temp['inclusive'] = array_shift($options);
-			}
+            if (!empty($options)) {
+                $temp['inclusive'] = array_shift($options);
+            }
 
-			$options = $temp;
-		}
+            $options = $temp;
+        }
 
-		if (!array_key_exists('token', $options)) {
-			throw new \Zend\Validator\Exception\InvalidArgumentException("Missing option 'token'");
-		}
+        if (!array_key_exists('max', $options)) {
+            throw new \Zend\Validator\Exception\InvalidArgumentException("Missing option 'max'");
+        }
 
-		if (!array_key_exists('inclusive', $options)) {
-			$options['inclusive'] = false;
-		}
+        if (!array_key_exists('inclusive', $options)) {
+            $options['inclusive'] = false;
+        }
 
-		$this->setToken($options['token'])
-			->setInclusive($options['inclusive']);
+        $this->setMax($options['max'])
+            ->setInclusive($options['inclusive']);
 
-		parent::__construct($options);
-	}
+        parent::__construct($options);
+    }
 
-	/**
-	 * Returns the max option
-	 *
-	 * @return mixed
-	 */
-	public function getToken()
-	{
-		return $this->token;
-	}
+    /**
+     * Returns the max option
+     *
+     * @return mixed
+     */
+    public function getMax()
+    {
+        return $this->max;
+    }
 
-	/**
-	 * Sets the max option
-	 *
-	 * @param  mixed $max
-	 * @return LessThan Provides a fluent interface
-	 */
-	public function setToken($max)
-	{
-		$this->token = $max;
-		return $this;
-	}
+    /**
+     * Sets the max option
+     *
+     * @param  mixed $max
+     * @return LessThan Provides a fluent interface
+     */
+    public function setMax($max)
+    {
+        $this->max = $max;
+        return $this;
+    }
 
-	/**
-	 * Returns the inclusive option
-	 *
-	 * @return boolean
-	 */
-	public function getInclusive()
-	{
-		return $this->inclusive;
-	}
+    /**
+     * Returns the inclusive option
+     *
+     * @return boolean
+     */
+    public function getInclusive()
+    {
+        return $this->inclusive;
+    }
 
-	/**
-	 * Sets the inclusive option
-	 *
-	 * @param  boolean $inclusive
-	 * @return LessThan Provides a fluent interface
-	 */
-	public function setInclusive($inclusive)
-	{
-		$this->inclusive = $inclusive;
-		return $this;
-	}
+    /**
+     * Sets the inclusive option
+     *
+     * @param  boolean $inclusive
+     * @return LessThan Provides a fluent interface
+     */
+    public function setInclusive($inclusive)
+    {
+        $this->inclusive = $inclusive;
+        return $this;
+    }
 
-	/**
-	 * Returns true if and only if $value is less than max option, inclusively
-	 * when the inclusive option is true
-	 *
-	 * @param  mixed $value
-	 * @return boolean
-	 */
-	public function isValid($value, $context = null)
-	{
-		$this->setValue($value);
+    /**
+     * Returns true if and only if $value is less than max option, inclusively
+     * when the inclusive option is true
+     *
+     * @param  mixed $value
+     * @return boolean
+     */
+    public function isValid($value)
+    {
+        $this->setValue($value);
 
-		if (null !== $context && !strtotime($this->token) && array_key_exists($this->token, $context)) {
-			$this->token = $context[$this->token];
-		}
+        if ($this->inclusive) {
+            if (strtotime($value) > strtotime($this->max)) {
+                $this->error(self::NOT_LESS_INCLUSIVE);
+                return false;
+            }
+        } else {
+            if (strtotime($value) >= strtotime($this->max)) {
+                $this->error(self::NOT_LESS);
+                return false;
+            }
+        }
 
-		if (empty($this->token)) {
-			return true;
-		}
-
-		if ($this->inclusive) {
-			if (strtotime($value) > strtotime($this->token)) {
-				$this->error(self::NOT_LESS_INCLUSIVE);
-				return false;
-			}
-		} else {
-			if (strtotime($value) >= strtotime($this->token)) {
-				$this->error(self::NOT_LESS);
-				return false;
-			}
-		}
-
-		return true;
+        return true;
     }
 }
