77066b6d2ef6e737afd435d945427833248f46fa
Pavel Fedák <pavel.fedak@bcom.cz>
2019-11-21 15:25:13 +0100

    StringContains

--- src/LemoBase/Validator/StringContains.php
+++ src/LemoBase/Validator/StringContains.php
@@ -6,15 +6,23 @@ use Zend\Validator\AbstractValidator;
 
 class StringContains extends AbstractValidator
 {
-    const NO_ALPHA          = 'noAlpha';
-    const NO_CAPITAL_LETTER = 'noCapitalLetter';
-    const NO_NUMERIC        = 'noNumeric';
-    const NO_SMALL_LETTER   = 'noSmallLetter';
+    const NO_ALPHA            = 'noAlpha';
+    const NO_VALID_CHARACTERS = 'noValidCharacters';
+    const NO_CAPITAL_LETTER   = 'noCapitalLetter';
+    const NO_NUMERIC          = 'noNumeric';
+    const NO_SMALL_LETTER     = 'noSmallLetter';
+
+    /**
+     * @var string|int|null
+     * string - valid characters
+     * int - 1 .. 128 ASCII characters
+     */
+    protected $cahracters = null;
 
     /**
      * @var bool
      */
-    protected $requireAlpha         = true;
+    protected $requireAlpha = false;
 
     /**
      * @var bool
@@ -24,26 +32,40 @@ class StringContains extends AbstractValidator
     /**
      * @var bool
      */
-    protected $requireNumeric       = true;
+    protected $requireNumeric = false;
 
     /**
      * @var bool
      */
-    protected $requireSmallLetter   = false;
+    protected $requireSmallLetter = false;
 
     /**
      * @var array
      */
     protected $messageTemplates = [
-        self::NO_ALPHA          => 'Value must contain at least one alphabetic character',
-        self::NO_CAPITAL_LETTER => 'Value must contain at least one capital letter',
-        self::NO_NUMERIC        => 'Value must contain at least one numeric character',
-        self::NO_SMALL_LETTER   => 'Value must contain at least one small letter',
+        self::NO_ALPHA            => 'Value must contain at least one alphabetic character',
+        self::NO_VALID_CHARACTERS => 'The input contains an invalid characters',
+        self::NO_CAPITAL_LETTER   => 'Value must contain at least one capital letter',
+        self::NO_NUMERIC          => 'Value must contain at least one numeric character',
+        self::NO_SMALL_LETTER     => 'Value must contain at least one small letter',
     ];
 
     public function __construct(array $options = null)
     {
         if (null !== $options) {
+
+            if (!empty($options['characters'])) {
+                if (
+                    is_int($options['characters'])
+                    && $options['characters'] > 0
+                    && $options['characters'] <= 128
+                    ||
+                    is_string($options['characters'])
+                ) {
+                    $this->cahracters = $options['characters'];
+                }
+            }
+
             if (isset($options['requireAlpha'])) {
                 $this->requireAlpha = (bool) $options['requireAlpha'];
             }
@@ -79,6 +101,25 @@ class StringContains extends AbstractValidator
         if (true === $this->requireAlpha && 0 == preg_match('/[a-z]/i', $value)) {
             $this->error(self::NO_ALPHA);
             return false;
+        }
+
+        // ASCII characters or allowed cahracters
+        if (null !== $this->cahracters) {
+            if (is_int($this->cahracters)) {
+                for ($x = 0; $x < $this->cahracters; ++$x) {
+                    $value = str_replace(chr($x), '', $value);
+                }
+            } else {
+                $chars = str_split($this->cahracters);
+                foreach ($chars as $char) {
+                    $value = str_replace($char, '', $value);
+                }
+            }
+
+            if (strlen($value) > 0) {
+                $this->error(self::NO_VALID_CHARACTERS);
+                return false;
+            }
         }
 
         // Capital letter
