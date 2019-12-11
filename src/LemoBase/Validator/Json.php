d85fd08c41c95f562a9fe37b4b8bc2f2b38dda6a
Pavel Fedák <pavel.fedak@bcom.cz>
2019-12-11 13:12:40 +0100

    Validator Json fix again

--- src/LemoBase/Validator/Json.php
+++ src/LemoBase/Validator/Json.php
@@ -44,10 +44,15 @@ class Json extends AbstractValidator
         }
 
         json_decode($value);
-        $errorMessage = json_last_error_msg();
-        if (!empty($errorMessage)) {
-            $this->reason = $errorMessage;
+        if (JSON_ERROR_NONE !== json_last_error()) {
             $this->error(self::INVALID);
+            $errorMessage = json_last_error_msg();
+            if (!empty($errorMessage)) {
+                $this->reason = $errorMessage;
+            } else {
+                $this->reason = 'An Unexpected Error Occurred';
+            }
+
             return false;
         }
 
